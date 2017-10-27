35c43ce905e37ac4ce529ec0289ecf944f8b1cc5
Metin Şaylan <metinsaylan@gmail.com>
2017-10-27 09:14:17 +0300

    rename functions and translation string

--- functions.php
+++ functions.php
@@ -3,21 +3,21 @@
 include_once( 'class-wp_bootstrap_navwalker.php' );
 include_once( 'shortcode-query_posts.php' ); 
 
-function setup_theme(){
+function kafka_theme_setup(){
 	
   add_theme_support( 'custom-background' );
   add_theme_support( 'nav-menus' );
   add_theme_support( 'title-tag' );
 	
 	register_nav_menus( array(
-        'primary' => __( 'Primary Menu', 'bs-cover' ),
+        'primary' => __( 'Primary Menu', 'kafka' ),
 	) );
   
   
   register_sidebar( array(
-    'name' => __( 'Main Sidebar', 'bs-cover' ),
+    'name' => __( 'Main Sidebar', 'kafka' ),
     'id' => 'sidebar-main',
-    'description' => __( 'Widgets in this area will be shown on all posts and pages.', 'bs-cover' ),
+    'description' => __( 'Widgets in this area will be shown on all posts and pages.', 'kafka' ),
     'before_widget' => '<div id="%1$s" class="widget border shadow pad mb %2$s">',
     'after_widget'  => '</div>',
     'before_title'  => '<h4 class="widget-title">',
@@ -25,12 +25,10 @@ function setup_theme(){
   ) );
   
 	
-} add_action( 'after_setup_theme', 'setup_theme' );
+} add_action( 'after_setup_theme', 'kafka_theme_setup' );
 
-function theme_scripts() {
+function kafka_theme_scripts() {
 
-  //wp_deregister_script( 'jquery' );
-  //wp_register_script( 'jquery', 'https://code.jquery.com/jquery-3.2.1.min.js', array(), null, true );
   wp_register_script( 'popper', get_template_directory_uri() . '/assets/popper.min.js', array( ), null, true );
   wp_register_script( 'bootstrap', 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta/js/bootstrap.min.js', array( 'popper' ), null, true );
 
@@ -39,9 +37,9 @@ function theme_scripts() {
   wp_enqueue_script( 'bootstrap' );
   
 }
-add_action( 'wp_enqueue_scripts', 'theme_scripts' );
+add_action( 'wp_enqueue_scripts', 'kafka_theme_scripts' );
 
-function wpdocs_custom_excerpt_length( $length ) {
+function kafka_excerpt_length( $length ) {
     return 20;
 }
-add_filter( 'excerpt_length', 'wpdocs_custom_excerpt_length', 999 );
+add_filter( 'excerpt_length', 'kafka_excerpt_length', 999 );

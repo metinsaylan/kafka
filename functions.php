d12a1b4471cad5b1a83eca8de9c0ccbf9fa634f0
Metin Şaylan <metinsaylan@gmail.com>
2017-11-01 23:53:49 +0300

    use theme bootstrap js

--- functions.php
+++ functions.php
@@ -1,19 +1,19 @@
 <?php
 
 include_once( 'inc/class-wp_bootstrap_navwalker.php' );
-include_once( 'inc/shortcode-query_posts.php' ); 
+include_once( 'inc/shortcode-query_posts.php' );
 
 function kafka_theme_setup(){
-	
+
   add_theme_support( 'custom-background' );
   add_theme_support( 'nav-menus' );
   add_theme_support( 'title-tag' );
-	
+
 	register_nav_menus( array(
         'primary' => __( 'Primary Menu', 'kafka' ),
 	) );
-  
-  
+
+
   register_sidebar( array(
     'name' => __( 'Main Sidebar', 'kafka' ),
     'id' => 'sidebar-main',
@@ -23,19 +23,19 @@ function kafka_theme_setup(){
     'before_title'  => '<h4 class="widget-title">',
     'after_title'   => '</h4>',
   ) );
-  
-	
+
+
 } add_action( 'after_setup_theme', 'kafka_theme_setup' );
 
 function kafka_theme_scripts() {
 
-  wp_register_script( 'popper', get_template_directory_uri() . '/assets/popper.min.js', array( ), null, true );
-  wp_register_script( 'bootstrap', 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta/js/bootstrap.min.js', array( 'popper' ), null, true );
+  /*wp_register_script( 'popper', get_template_directory_uri() . '/assets/popper.min.js', array( ), null, true );
+  wp_register_script( 'bootstrap', get_template_directory_uri() . '/assets/bootstrap.min.js', array( 'popper' ), null, true );
 
   wp_enqueue_script( 'jquery' );
   wp_enqueue_script( 'popper' );
-  wp_enqueue_script( 'bootstrap' );
-  
+  wp_enqueue_script( 'bootstrap' );*/
+
 }
 add_action( 'wp_enqueue_scripts', 'kafka_theme_scripts' );
 

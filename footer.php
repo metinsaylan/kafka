20554a658b9286ef4b61dd91f7d67baf59471705
Metin Şaylan <metinsaylan@gmail.com>
2017-11-01 23:52:23 +0300

    add theme credits

--- footer.php
+++ footer.php
@@ -1,10 +1,11 @@
 </div>
 
-<footer class="mastfoot">
-<div class="container text-center">
-  <p class="text-muted">&copy; 2017 - <?php bloginfo('name'); ?></p>
+<footer class="footer">
+<div class="container text-center ">
+  <p class="muted">&copy; 2017 - <a href="<?php bloginfo('url'); ?>" class="muted"><?php bloginfo('name'); ?></a></p>
+  <p class="small theme-credits muted">Powered by <a href="https://wordpress.org/" target="_blank">WordPress</a> and <a href="http://metinsaylan.com/kafka-wordpress-theme/" target="_blank">Kafka</a></p>
 </div>
-  
+
 <?php wp_footer();?>
 
 </footer>

cee3c5eb47e126ebeacf143aee2133d391a1e32a
Metin Şaylan <metinsaylan@gmail.com>
2017-11-01 23:53:35 +0300

    add cf to entry

--- single.php
+++ single.php
@@ -4,29 +4,29 @@
 <?php
 
 if( have_posts() ){
-  
+
   while( have_posts() ):
   the_post();
-  
+
 ?>
-<div class="h-entry">
-  <header class="entry-header"> 
+<div class="h-entry cf">
+  <header class="entry-header">
   <h1 class="entry-title p-name"><a href="<?php the_permalink(); ?>" rel="bookmark" class="u-url"><?php the_title(); ?></a></h1>
-	<p class="post-meta"> 
+	<p class="post-meta">
 	  <span class="post-categories"><?php the_category(' '); ?></span>
-	  <span class="separator muted"> // </span> 
-	  <time class="post-date dt-published" datetime="<?php the_time('c'); ?>"><?php the_date('F j, Y'); ?></time> 
-	  <span class="separator muted"> // </span> 
-	  <span class="entry-author"><a class="p-author h-card" href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ), get_the_author_meta( 'user_nicename' ) ); ?>"><?php the_author(); ?></a></span> 
+	  <span class="separator muted"> // </span>
+	  <time class="post-date dt-published" datetime="<?php the_time('c'); ?>"><?php the_date('F j, Y'); ?></time>
+	  <span class="separator muted"> // </span>
+	  <span class="entry-author"><a class="p-author h-card" href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ), get_the_author_meta( 'user_nicename' ) ); ?>"><?php the_author(); ?></a></span>
 	  <div class="post-tags"><?php the_tags( '<span class="tag">#', '</span><span class="tag">#', '</span>' ); ?></div>
 	</p>
   </header>
-  <div class="page-content e-content" role="main"> 
+  <div class="page-content e-content" role="main">
     <?php the_content(); ?>
   </div>
-</div> 
+</div>
 <?php get_template_part('nav', 'post'); ?>
-<?php 
+<?php
 
 	endwhile;
 }

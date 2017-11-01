b4a999486573a11e607717a6a86e1d1147a49f42
Metin Şaylan <metinsaylan@gmail.com>
2017-11-02 00:09:35 +0300

    move wp_head hook call

--- header.php
+++ header.php
@@ -5,8 +5,8 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta http-equiv="content-type" content="text/html; charset=utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1">
+  <?php wp_head();?>
 	<style> <?php include 'theme.min.css'; ?> </style>
-<?php wp_head();?>
   </head>
 <body <?php body_class(); ?>>
 

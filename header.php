6ad87615fd522c981113a7b88fd3dd278bc06090
Metin Şaylan <metinsaylan@gmail.com>
2017-11-02 00:01:48 +0300

    remove fixed styling

--- header.php
+++ header.php
@@ -5,8 +5,6 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta http-equiv="content-type" content="text/html; charset=utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1">
-	<link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/paper/bootstrap.min.css" rel="stylesheet">
-	<link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css" rel="stylesheet">
 	<style> <?php include 'theme.min.css'; ?> </style>
 <?php wp_head();?>
   </head>
@@ -15,7 +13,7 @@
   <nav class="navbar navbar-default animated fadeInDown">
 	<div class="container">
 	  <div class="navbar-header">
-		
+
 		<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navmain" aria-expanded="false" aria-controls="navbar">
 		  <span class="sr-only">Toggle navigation</span>
 		  <span class="icon-bar"></span>
@@ -23,9 +21,9 @@
 		  <span class="icon-bar"></span>
 		</button>
 		<a class="navbar-brand" href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>
-		
+
 	  </div>
-	  
+
 	  <?php
             wp_nav_menu( array(
                 'menu'              => 'primary',
@@ -35,15 +33,15 @@
                 'container_class'   => 'collapse navbar-collapse',
                 'container_id'      => 'navmain',
                 'menu_class'        => 'nav navbar-nav',
-			  
-			  'items_wrap' => '<ul class="nav navbar-nav navbar-right">%3$s</ul>', 
-			  
+
+			  'items_wrap' => '<ul class="nav navbar-nav navbar-right">%3$s</ul>',
+
                 'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
                 'walker'            => new WP_Bootstrap_Navwalker())
             );
         ?>
-	  	  
+
 	</div>
   </nav>
-  
-<div class="container">          
+
+<div class="container">

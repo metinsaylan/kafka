588c3375f7dfdbdb04d10c1be874f28d5a1ad7fc
Metin Şaylan <metinsaylan@gmail.com>
2018-02-27 22:12:08 +0300

    add search header and banners to search results

--- search.php
+++ search.php
@@ -1,12 +1,28 @@
 <?php get_header(); ?>
 <div class="row">
   <div class="col-md-8">	
-<?php if( have_posts() ){ while( have_posts() ): the_post(); ?>
+  
+<div class="page-header">
+Search results for <strong><?php echo get_search_query(); ?></strong>
+</div>
+
+<?php if( have_posts() ){ $ctr = 0; while( have_posts() ): the_post(); $ctr++; 
+
+if( $ctr == 2 || $ctr == 5 ){
+	get_template_part( 'banner', 'post' );
+}
+
+?>
 	<div <?php post_class('listing border pad shadow mb'); ?>>
 	  <h6 class="entry-title"><a rel="bookmark" class="url" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h6>
       <?php the_excerpt(); ?>
 	</div>
-<?php endwhile; } ?>
+<?php endwhile; ?>
+
+<?php get_template_part( 'banner', 'post' ); ?>
+<?php get_template_part('nav', 'blog'); 
+
+} else { get_template_part('content', '404'); } ?>
   </div>
   <?php get_sidebar(); ?>
 </div>

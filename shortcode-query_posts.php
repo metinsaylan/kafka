c650a86114bea955daddfe8a5404727f34c5981a
Metin Şaylan <metinsaylan@gmail.com>
2018-02-27 22:12:24 +0300

    add query posts shortcode

---
+++ shortcode-query_posts.php
@@ -0,0 +1,56 @@
+<?php 
+
+function stf_queryposts($atts){
+
+	extract(shortcode_atts( array(
+	'category_id' => '',
+	'category_name' => '',
+	'tag' => '',
+	'day' => '',
+	'month' => '',
+	'year' => '',
+	'count' => '-1', 
+	'author_id' => '',
+	'author_name' => '',
+	'order_by' => 'date',
+	), $atts));
+	
+	$output = '';
+	$query = array();
+	
+	if ($category_id != '') $query[] = 'cat=' .$category_id;
+	if ($category_name != '') $query[] = 'category_name=' .$category_name;
+	if ($tag != '') $query[] = 'tag=' . $tag;
+	if ($day != '') $query[] = 'day=' . $day;
+	if ($month != '') $query[] = 'monthnum=' . $month;
+	if ($year != '') $query[] = 'year=' . $year;
+	if ($count) $query[] = 'posts_per_page=' .$count;
+	if ($author_id != '') $query[] = 'author=' . $author_id;
+	if ($author_name != '') $query[] = 'author_name=' . $author_name;
+	if ($order_by) $query[] = 'orderby=' . $order_by;
+
+	$backup = $post;
+
+	$posts = new WP_Query(implode('&',$query));
+
+	$output = '';
+	$temp_title = '';
+	$temp_link = '';
+  
+	if ($posts->have_posts()):
+		$output = '<ul>';
+		while ($posts->have_posts()):
+			$posts->the_post();
+			$temp_title = get_the_title($post->ID);
+			$temp_link = get_permalink($post->ID);
+			$output .= "<li><a href='$temp_link'>$temp_title</a></li>";
+		endwhile;
+		$output .= '</ul>';
+	else:
+		$output .= '<p class="error">[query] '.__("No posts found matching the arguments", 'stf').'</p>';
+	endif;
+	
+	wp_reset_postdata();
+	return $output;
+  
+} add_shortcode('query_posts', 'stf_queryposts');

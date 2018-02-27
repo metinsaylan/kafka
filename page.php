760bef69a0903ef3ad1f6da4e14825208a84fb79
Metin Şaylan <metinsaylan@gmail.com>
2018-02-27 22:15:06 +0300

    +excerpt to page header, +page banners, +comments

--- page.php
+++ page.php
@@ -12,11 +12,24 @@ if( have_posts() ){
 <div class="h-entry">
   <header class="entry-header"> 
   <h1 class="entry-title p-name"><a href="<?php the_permalink(); ?>" rel="bookmark" class="u-url"><?php the_title(); ?></a></h1>
+  <?php if( has_excerpt() ): ?>
+	<p class="lead"><?php echo wp_strip_all_tags( get_the_excerpt(), true ); ?></p>
+  <?php endif; ?>
   </header>
   <div class="page-content e-content" role="main"> 
+	<?php get_template_part( 'banner', 'page' ) ?>
     <?php the_content(); ?>
+	<?php get_template_part( 'banner', 'page' ) ?>
   </div>
 </div>
+
+<?php 
+ if ( comments_open() || get_comments_number() ) :
+    comments_template();
+ endif;
+?>
+
+
 <?php 
 
 	endwhile;

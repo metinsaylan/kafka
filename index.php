862a207d72217c7bf779df9d3d7a4dffe35719d9
Metin Şaylan <metinsaylan@gmail.com>
2018-02-27 23:20:25 +0300

    +home banners

--- index.php
+++ index.php
@@ -2,14 +2,26 @@
 <div class="row">
   <div class="col-md-8">
 	
-<?php if( have_posts() ){ while( have_posts() ): the_post(); ?>
+<?php if( have_posts() ){ $ctr = 0; while( have_posts() ): the_post(); $ctr++; 
+
+if( $ctr == 2 || $ctr == 5 ){
+	get_template_part( 'banner', 'post' );
+}
+
+?>
   
 	<div class="entry border shadow pad mb">
 	  <h3 class="entry-title"><a rel="bookmark" class="url" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
       <?php the_excerpt(); ?>
 	</div>
  
-<?php endwhile; get_template_part('nav', 'blog'); } ?>
+<?php endwhile; ?>
+
+<?php get_template_part( 'banner', 'post' ); ?>
+<?php get_template_part('nav', 'blog'); 
+
+} ?>
+
   </div>
   <?php get_sidebar(); ?>
 </div>

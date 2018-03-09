d6cdae0fe41764e153349e7c8f02d7799bfbc813
Metin Şaylan <metinsaylan@gmail.com>
2018-03-09 21:59:03 +0300

    add footer sidebar, update credits text

--- footer.php
+++ footer.php
@@ -1,12 +1,10 @@
 </div>
 
 <footer class="footer">
-<div class="container text-center ">
-  <p class="muted">&copy; 2017 - <a href="<?php bloginfo('url'); ?>" class="muted"><?php bloginfo('name'); ?></a></p>
-  <p class="small theme-credits muted">Powered by <a href="https://wordpress.org/" target="_blank">WordPress</a> and <a href="http://metinsaylan.com/kafka-wordpress-theme/" target="_blank">Kafka</a></p>
-</div>
-
-<?php wp_footer();?>
-
-</footer>
-</body></html>
+<?php if( is_active_sidebar('sidebar-footer') ): ?>
+<div class="container"><div class="row">
+<?php dynamic_sidebar('sidebar-footer'); ?>
+</div></div>
+<?php endif; ?>
+<div class="container text-center "><p class="muted">&copy; 2017 - <a href="<?php bloginfo('url'); ?>" class="muted"><?php bloginfo('name'); ?></a></p><p class="small theme-credits muted"><a href="https://wordpress.org/" target="_blank">Ⓦ</a> <a href="http://metinsaylan.com/kafka-wordpress-theme/" title="A WordPress theme by metinsaylan" target="_blank">KΛfKΛ</a></p></div>
+<?php wp_footer();?> </footer> </body> </html>

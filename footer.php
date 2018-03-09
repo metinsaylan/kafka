b51ea9b3345ad59a2a190bf493c4c1b9ed290d02
Metin Şaylan <metinsaylan@gmail.com>
2018-03-10 00:02:48 +0300

    update footer text

--- footer.php
+++ footer.php
@@ -6,5 +6,11 @@
 <?php dynamic_sidebar('sidebar-footer'); ?>
 </div></div>
 <?php endif; ?>
-<div class="container text-center "><p class="muted">&copy; 2017 - <a href="<?php bloginfo('url'); ?>" class="muted"><?php bloginfo('name'); ?></a></p><p class="small theme-credits muted"><a href="https://wordpress.org/" target="_blank">Ⓦ</a> <a href="http://metinsaylan.com/kafka-wordpress-theme/" title="A WordPress theme by metinsaylan" target="_blank">KΛfKΛ</a></p></div>
+<div class="container text-center ">
+  <p class="muted">&copy; 2017 - <a href="<?php bloginfo('url'); ?>" class="muted"><?php bloginfo('name'); ?></a>
+  </p>
+  <p class="small theme-credits muted">
+    <a href="https://wordpress.org/" target="_blank">Ⓦ</a> ＋ <a href="http://metinsaylan.com/kafka-wordpress-theme/" title="A WordPress theme by metinsaylan" target="_blank">KΛfKΛ</a>
+  </p>
+</div>
 <?php wp_footer();?> </footer> </body> </html>

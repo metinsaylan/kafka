2c2edca510c5b9fdc55ef1c24da7e0fdb800db13
Metin Şaylan <metinsaylan@gmail.com>
2018-03-10 16:07:12 +0300

    add ad code to sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,13 @@
 <div class="col-md-4">
 <aside id="sidebar" class="" role="complementary">
-  <?php dynamic_sidebar('sidebar-main'); ?>
+<div class="widget widget_search">
+<?php get_search_form(); ?>
+</div>
+
+<div class="banner mb">
+<?php $sidebar_adcode = get_theme_mod( 'kafka_sidebar_adcode' ); echo $sidebar_adcode; ?>
+</div>
+
+<?php dynamic_sidebar('sidebar-main'); ?>
 </aside>
 </div>

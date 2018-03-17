ae7586b27333ac35c50ba713f925a012b1a09807
Metin Şaylan <metinsaylan@gmail.com>
2018-03-17 23:08:21 +0300

    add footer text customization

--- footer.php
+++ footer.php
@@ -5,9 +5,13 @@
 <div class="container"><div class="row">
 <?php dynamic_sidebar('sidebar-footer'); ?>
 </div></div>
+<hr />
 <?php endif; ?>
 <div class="container text-center ">
-  <p class="muted">&copy; 2017 - <a href="<?php bloginfo('url'); ?>" class="muted"><?php bloginfo('name'); ?></a>
+  <p id="footer-text" class="muted"><?php
+    $footertext = get_theme_mod( 'kafka_footer_text', '&copy; 2018  <a href="' . get_bloginfo( 'url' )  . '" class="muted">' . get_bloginfo( 'name' ) . '</a> - ' . get_bloginfo( 'description' ) );
+    echo $footertext;
+    ?></a>
   </p>
   <p class="small theme-credits muted">
     <a href="https://wordpress.org/" target="_blank">Ⓦ</a> ＋ <a href="http://metinsaylan.com/kafka-wordpress-theme/" title="A WordPress theme by metinsaylan" target="_blank">KΛfKΛ</a>

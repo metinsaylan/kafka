3f09713c3f283f62c109616a7fffaa3bf0d33862
Metin Saylan <metinsaylan@gmail.com>
2018-05-11 14:31:17 +0300

    add footer menu

--- footer.php
+++ footer.php
@@ -8,6 +8,26 @@
 </div></div>
 <?php endif; ?>
 
+<div class="container text-center "><div class="row">
+<?php
+	wp_nav_menu( array(
+		'menu'              => 'footer',
+		'theme_location'    => 'footer',
+		'depth'             => 0,
+		'container'         => 'nav',
+		'container_class'   => '',
+		'container_id'      => 'navfooter',
+		'menu_class'        => 'footer-menu',
+
+        'items_wrap' => '<ul class="list-inline muted">%3$s</ul>',
+		'item_spacing' => 'discard',
+
+		'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+		'walker'            => new WP_Bootstrap_Navwalker())
+	);
+?>
+</div></div>
+
 <div class="container text-center ">
   <p id="footer-text" class="muted"><?php echo get_theme_mod( 'kafka_footer_text', '&copy; 2018  <a href="' . get_bloginfo( 'url' )  . '" class="muted">' . get_bloginfo( 'name' ) . '</a> - ' . get_bloginfo( 'description' ) ); ?>
   </p>

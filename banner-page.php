930ee6ba50738671dc49a0f728b16b4515cc2fbc
Metin Saylan <metinsaylan@gmail.com>
2018-05-11 10:29:27 +0300

    simplify

--- banner-page.php
+++ banner-page.php
@@ -1,3 +1,1 @@
-<div class="banner mb">
-<?php $sidebar_adcode = get_theme_mod( 'kafka_banner_adcode' ); echo $sidebar_adcode; ?>
-</div>
+<div class="banner mb"> <?php echo get_theme_mod( 'kafka_banner_adcode', '' ); ?> </div>

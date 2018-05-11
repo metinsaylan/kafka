f19ffdbc11e4ee0d7b3a86ebb1c99f2901c5498d
Metin Saylan <metinsaylan@gmail.com>
2018-05-11 10:26:45 +0300

    tidy up

--- inc/filter-title_separator.php
+++ inc/filter-title_separator.php
@@ -1,6 +1,5 @@
 <?php 
-
-function kafka_document_title_separator(  $sep ) {
-    return "//";
-}
+function kafka_document_title_separator(  $sep ) {
+    return "//";
+}
 add_filter( 'document_title_separator', 'kafka_document_title_separator', 10, 1 );

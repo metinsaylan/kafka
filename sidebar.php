c33f21406074c1c83d7f400d55ac9d0ddb7066ba
Metin Şaylan <metinsaylan@gmail.com>
2018-05-18 23:05:54 +0300

    add lb

--- sidebar.php
+++ sidebar.php
@@ -1,13 +1,15 @@
 <div class="col-md-4"> 
 <aside id="sidebar" class="sidebar"> 
+
 <div class="widget widget_search shadow mb">
 <?php get_search_form(); ?>
 </div>
-<div class="banner banner-fixed"> 
 
+<div class="banner banner-fixed"> 
 <?php $sidebar_adcode = get_theme_mod( 'kafka_sidebar_adcode' ); echo $sidebar_adcode; ?>
 </div>
 
 <?php dynamic_sidebar('sidebar-main'); ?>
+
 </aside>
 </div>

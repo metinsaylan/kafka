9bfdbfa96fd7c857c8a6ae58e8dab7b30fb092c2
Metin Şaylan <metinsaylan@gmail.com>
2018-05-18 23:46:40 +0300

    move ad area after dynamic sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,15 +1,11 @@
 <div class="col-md-4"> 
 <aside id="sidebar" class="sidebar"> 
-
 <div class="widget widget_search shadow mb">
 <?php get_search_form(); ?>
 </div>
-
+<?php dynamic_sidebar('sidebar-main'); ?>
 <div class="banner banner-fixed"> 
 <?php $sidebar_adcode = get_theme_mod( 'kafka_sidebar_adcode' ); echo $sidebar_adcode; ?>
 </div>
-
-<?php dynamic_sidebar('sidebar-main'); ?>
-
 </aside>
 </div>

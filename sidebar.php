35ee39a5dfcd65e0180cb566265b90f4725074ec
Metin Şaylan <metinsaylan@gmail.com>
2018-05-18 23:04:49 +0300

    fix sidebar banner

--- sidebar.php
+++ sidebar.php
@@ -1,10 +1,10 @@
-<div class="col-md-4">
-<aside id="sidebar" class="sidebar">
-<div class="widget widget_search shadow">
+<div class="col-md-4"> 
+<aside id="sidebar" class="sidebar"> 
+<div class="widget widget_search shadow mb">
 <?php get_search_form(); ?>
 </div>
+<div class="banner banner-fixed"> 
 
-<div class="banner mb">
 <?php $sidebar_adcode = get_theme_mod( 'kafka_sidebar_adcode' ); echo $sidebar_adcode; ?>
 </div>
 

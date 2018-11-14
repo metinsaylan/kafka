7bbf7bf1a684a26a0a4ef7858913baff34b43324
Metin Saylan <metinsaylan@gmail.com>
2018-11-14 16:34:20 +0300

    rename micro data

--- search.php
+++ search.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 <div class="row">
 <div class="col-md-8">
-<h1 class="entry-title">Search results for <strong><?php echo get_search_query(); ?></strong></h1>
+<h1 class="p-name">Search results for <strong><?php echo get_search_query(); ?></strong></h1>
 <?php get_template_part( 'partials/loop' ); ?>
 </div>
 <?php get_sidebar(); ?>
